6f5564831a18c9df7685cf4b099e301b86ac154d
Jaijiv <jaijiv@yahoo.com>
2014-10-10 14:44:55 -0700

    added some test go files to check travis

---
+++ social.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Started social app...")
+}
+
+func TestMe() int {
+	return 1
+}
---
+++ social_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func TestTestMe(t *testing.T) {
+	log.Println("Started social app...")
+}
